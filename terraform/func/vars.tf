f6c1c8b4e3d02e987552bcccddae6560d1146198
Gustav Westling <gustav@westling.dev>
2019-11-17 15:20:25 +0100

    terraform: create reusable func module, build score/v2

---
+++ terraform/func/vars.tf
@@ -0,0 +1,27 @@
+variable "work_dir" {
+ type = string
+}
+
+variable "name" {
+ type = string
+}
+
+variable "aws_iam_role_arn" {
+    type = string
+}
+
+variable "aws_region_name" {
+    type = string
+}
+
+variable "rest_api_id" {
+    type = string
+}
+
+variable "rest_api_parent_id" {
+    type = string
+}
+
+variable "root" {
+    type = string
+}
--- terraform/main.tf
+++ terraform/main.tf
@@ -28,3 +28,14 @@ terraform {
 
 data "aws_region" "current" {
 }
+
+module "score_v2" {
+  source = "./func"
+  work_dir = "../funcs/score/v2"
+  name = "score_v2"
+  aws_iam_role_arn = aws_iam_role.kube_score_lambda.arn
+  aws_region_name = data.aws_region.current.name
+  rest_api_id = aws_api_gateway_rest_api.kube_score_api.id
+  rest_api_parent_id   = aws_api_gateway_rest_api.kube_score_api.root_resource_id
+  root = abspath("..")
+}
