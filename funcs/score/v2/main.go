de3ee8077b923a3cc0037d798ee00dddfc1f9d44
Gustav Westling <gustav@westling.dev>
2019-11-17 15:20:47 +0100

    funcs/score/v2: new module with terminal like output

---
+++ funcs/score/v2/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"io"
+	"io/ioutil"
+	"net/http"
+	"strings"
+
+	"github.com/zegl/kube-score/config"
+	"github.com/zegl/kube-score/parser"
+	"github.com/zegl/kube-score/renderer/human"
+	"github.com/zegl/kube-score/score"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+
+	t2html "github.com/buildkite/terminal-to-html"
+
+	"github.com/fatih/color"
+)
+
+func handleRequest(req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	fail := func(err error) events.APIGatewayProxyResponse {
+		return events.APIGatewayProxyResponse{StatusCode: http.StatusBadRequest, Body: err.Error()}
+	}
+
+	cnf := config.Configuration{
+		AllFiles: []io.Reader{
+			strings.NewReader(req.Body),
+		},
+	}
+
+	allObjs, err := parser.ParseFiles(cnf)
+	if err != nil {
+		return fail(err), nil
+	}
+
+	card, err := score.Score(allObjs, cnf)
+
+	color.NoColor = false
+	output := human.Human(card, 0, 100)
+
+	body, _ := ioutil.ReadAll(output)
+
+	htmlBody := t2html.Render(body)
+
+	res := events.APIGatewayProxyResponse{
+		StatusCode: http.StatusOK,
+		Headers: map[string]string{
+			"Content-Type":                 "text/html",
+			"Access-Control-Allow-Origin":  "*",
+			"Access-Control-Allow-Methods": "OPTIONS,POST",
+			"Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
+		},
+		Body: string(htmlBody),
+	}
+	return res, nil
+}
+
+func main() {
+	lambda.Start(handleRequest)
+}
--- go.mod
+++ go.mod
@@ -5,9 +5,11 @@ go 1.12
 require (
 	github.com/KyleBanks/depth v1.2.1 // indirect
 	github.com/aws/aws-lambda-go v1.11.1
+	github.com/buildkite/terminal-to-html v3.2.0+incompatible
 	github.com/davecheney/graphpkg v0.0.0-20160907084000-658e86e77795 // indirect
+	github.com/fatih/color v1.7.0
 	github.com/golang/glog v0.0.0-20160126235308-23def4e6c14b // indirect
 	github.com/kisielk/godepgraph v0.0.0-20190626013829-57a7e4a651a9 // indirect
 	github.com/pkg/browser v0.0.0-20180916011732-0a3d74bf9ce4 // indirect
-	github.com/zegl/kube-score v1.3.0
+	github.com/zegl/kube-score v1.3.1-0.20191117123807-298b67358dfa
 )
